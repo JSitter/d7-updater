bb1cb525cf73b6c4c1bb15653279ddc594b91b0a
Justin Sitter <suddenlysquirrels@yahoo.com>
2017-09-14 15:34:01 -0700

    Initial Commit

---
+++ d7_updater.py
@@ -0,0 +1,9 @@
+#This script written by Justin Sitter to update D7 websites without replacing sites folder
+#Version 0.1 Beta
+
+#Get current directory path
+
+#Get path to d7 zip file
+
+#get contents of prod root
+
